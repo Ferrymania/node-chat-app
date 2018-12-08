dfc12ffcae6354a1f8d80c96ad4754ef78db310b
Ferrymania <904655716@qq.com>
2018-12-08 09:31:27 +0800

    init commit

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const path = require('path');
+const express = require('express');
+const publicPath = path.join(__dirname,'../public');
+const port = process.env.PORT || 3000;
+
+let app = express();
+
+app.use(express.static(publicPath));
+app.get('/',(req,res)=>{
+    res.send('Hello chat');
+});
+
+app.listen(port,()=>{
+    console.log(`app is listing on port ${port}`);
+});
